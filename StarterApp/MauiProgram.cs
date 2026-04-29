b6f9505e8ec5b52e11659636d020f76b7ab65723
Max Kelly <max.k9994@gmail.com>
2026-04-29 19:19:15 +0000

    Added API-based authentication with JWT support

--- StarterApp/MauiProgram.cs
+++ StarterApp/MauiProgram.cs
@@ -2,7 +2,6 @@ using Microsoft.Extensions.Logging;
 using StarterApp.ViewModels;
 using StarterApp.Database.Data;
 using StarterApp.Views;
-using System.Diagnostics;
 using StarterApp.Services;
 
 namespace StarterApp;
@@ -12,6 +11,7 @@ public static class MauiProgram
     public static MauiApp CreateMauiApp()
     {
         var builder = MauiApp.CreateBuilder();
+
         builder
             .UseMauiApp<App>()
             .ConfigureFonts(fonts =>
@@ -20,25 +20,51 @@ public static class MauiProgram
                 fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
             });
 
-        builder.Services.AddDbContext<AppDbContext>();
+        // -------------------------------------------------------------------
+        // Toggle between API and local database authentication
+        // -------------------------------------------------------------------
+        const bool useSharedApi = true;
 
-        builder.Services.AddSingleton<IAuthenticationService, AuthenticationService>();
+        if (useSharedApi)
+        {
+            var httpClient = new HttpClient
+            {
+                BaseAddress = new Uri("https://set09102-api.b-davison.workers.dev/")
+            };
+
+            builder.Services.AddSingleton(httpClient);
+            builder.Services.AddSingleton<IAuthenticationService, ApiAuthenticationService>();
+        }
+        else
+        {
+            builder.Services.AddDbContext<AppDbContext>();
+            builder.Services.AddSingleton<IAuthenticationService, LocalAuthenticationService>();
+        }
+
+        // Navigation service
         builder.Services.AddSingleton<INavigationService, NavigationService>();
 
+        // App + Shell
         builder.Services.AddSingleton<AppShellViewModel>();
         builder.Services.AddSingleton<AppShell>();
         builder.Services.AddSingleton<App>();
 
+        // Pages & ViewModels
         builder.Services.AddTransient<MainViewModel>();
         builder.Services.AddTransient<MainPage>();
+
         builder.Services.AddSingleton<LoginViewModel>();
         builder.Services.AddTransient<LoginPage>();
+
         builder.Services.AddSingleton<RegisterViewModel>();
         builder.Services.AddTransient<RegisterPage>();
+
         builder.Services.AddTransient<UserListViewModel>();
         builder.Services.AddTransient<UserListPage>();
-        builder.Services.AddTransient<UserDetailPage>();
+
         builder.Services.AddTransient<UserDetailViewModel>();
+        builder.Services.AddTransient<UserDetailPage>();
+
         builder.Services.AddSingleton<TempViewModel>();
         builder.Services.AddTransient<TempPage>();
 

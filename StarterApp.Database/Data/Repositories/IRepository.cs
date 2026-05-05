ec52d1e7eaf2c68bc7ce50d73138bca73b6879c4
Max Kelly <max.k9994@gmail.com>
2026-05-05 21:55:52 +0000

    Add repository pattern for item data access

---
+++ StarterApp.Database/Data/Repositories/IRepository.cs
@@ -0,0 +1,10 @@
+namespace StarterApp.Database.Data.Repositories;
+
+public interface IRepository<T>
+{
+    Task<List<T>> GetAllAsync();
+    Task<T?> GetByIdAsync(int id);
+    Task<T?> AddAsync(T entity);
+    Task<bool> UpdateAsync(T entity);
+    Task<bool> DeleteAsync(int id);
+}
